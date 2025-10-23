52f43439aa52d6bfbee3f92911e5487c59da4382
svcan <scantari@terpmail.umd.edu>
2025-10-23 17:59:37 -0400

    Committed my base, contact, favorite-course, and jquery HTML's with new additions.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -23,5 +23,39 @@ def about_css():
 def greeting():
     return render_template('greeting.html')
 
+@app.route('/jquery')
+def jquery():
+    return render_template('jquery.html')
+
+@app.route("/favorite-course")
+def favorite_course1():
+    # read query parameters from URL: ?subject=ARTH&course_number=260C
+    subject = request.args.get("subject", "")
+    course_number = request.args.get("course_number", "")
+    return render_template("favorite-course.html", subject=subject, course_number=course_number)
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        # read form fields
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        extra_field = request.form.get("extra_field")
+        # show thank-you page with submitted values
+        return render_template(
+            "contact.html",
+            submitted=True,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            extra_field=extra_field
+        )
+    # show the form when GET
+    return render_template("contact.html", submitted=False)
+
+
+
 if __name__ == '__main__':
     app.run()
+
